908b309e15c437d168a6c0b83926da03730c02f0
Aditya Bhonsle <adityabhonsle@Adityas-MacBook-Pro.local>
2025-03-20 16:16:40 -0400

    created functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,70 @@
 // TODO: Include packages needed for this application
+import inquirer from "inquirer";
+import fs from "fs";
+import path from "path";
+import generateMarkdown from "./utils/generateMarkdown.js";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide a description of your project.",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How do you install dependencies?",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How can the user use this application?",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Select a license for your application.",
+        choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"],
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "How can users contribute to this application?",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "How can users test this application?",
+    },
+    {
+        type: "input",
+        name: "gitHub",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        writeToFile("README.md", generateMarkdown(answers));
+    });
+};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,65 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license === "None") {
+        return "";
+    }
+    return `![License](https://img.shields.io/badge/license-${license.replace(" ", "_")}-blue.svg)`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (license === "None") {
+        return "";
+    }
+    return `* [License](#license)`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (license === "None") {
+        return "";
+    }
+    return `## License
+This project is licensed under the ${license} license.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+    return `# ${data.title}${renderLicenseBadge(data.license)}
+
+  ## Description:
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Test](#test)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Test
+  ${data.test}
+
+  ## Questions
+  Should you have questions/concerns, please reach me via:
+  - GitHub: [${data.gitHub}](https://github.com/${data.gitHub})
+  - Email: [${data.email}](mailto:${data.email})
 
 `;
 }
